dda797a0ed6dd418ee0f0eecde0af5360d552579
Chirag Khatri <kchiragkhatri2001@gmail.com>
2023-03-12 15:22:06 +0530

    initial commit

---
+++ src/icons/icons.js
@@ -0,0 +1,29 @@
+import Activity from "./Activity.jsx";
+import Search from "./Search.jsx";
+import BuildingOne from "./Building.jsx";
+import Copy from "./Copy.jsx";
+import AtSign from "./AtSign.jsx";
+const icons = [
+  {
+    iconName: "building-01",
+    iconSrc: BuildingOne,
+  },
+
+  {
+    iconName: "copy",
+    iconSrc: Copy,
+  },
+  {
+    iconName: "search",
+    iconSrc: Search,
+  },
+  {
+    iconName: "activity",
+    iconSrc: Activity,
+  },
+  {
+    iconName: "at-sign",
+    iconSrc: AtSign,
+  },
+];
+export default icons;
